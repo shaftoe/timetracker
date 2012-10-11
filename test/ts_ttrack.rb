645de44056e22a6190d3e740feef3f96569cbd2c
Alexander Fortin <alexander.fortin@gmail.com>
2012-10-11 15:18:53 +0200

    New test cases

--- test/ts_ttrack.rb
+++ test/ts_ttrack.rb
@@ -1,19 +1,56 @@
 # -*- coding: utf-8 -*-
 require 'test/unit'
 require 'TTrack'
+require 'FileUtils'
 
 class TestTimeTracker < Test::Unit::TestCase
 
   def test_init
+    FileUtils.rm('testdb')  # Remove testing db before
     tt = TTrack.new 'testdb'
     assert_equal tt.class, TTrack
   end
 
+  def test_status
+    tt = TTrack.new 'testdb'
+    assert_equal nil, tt.status
+  end
+
   def test_start_stop
     tt = TTrack.new 'testdb'
-    assert_equal true, tt.start('issuename_test', 'notes_test')
-    assert_equal true, tt.stop
+    assert tt.start('issuename_test', 'notes_test')
+    assert tt.stop
     assert_equal false, tt.stop
+  end
+
+  def test_status_with_data_when_nothing_running
+    tt = TTrack.new 'testdb'
+    status = tt.status('issuename_test')
+    assert_equal 'issuename_test', status[:task]
+    assert_equal 0, status[:elapsed]
+    assert_equal Float, status[:elapsed_hours].class
+  end
+
+  def test_status_when_task_running
+    tt = TTrack.new 'testdb'
+    assert tt.start('issuename_test_status')
+
+    status = tt.status
+    assert_equal 'issuename_test_status', status[:task]
+  end
+
+  def test_status_with_old_tasks
+    tt = TTrack.new 'testdb'
+    status = tt.status('issuename_test')
+    assert_equal 'issuename_test', status[:task]
+    assert_equal 0, status[:elapsed]
+    assert_equal Float, status[:elapsed_hours].class
+  end
+
+  def test_status_wrong
+    tt = TTrack.new 'testdb'
+    status = tt.status('foo_issuename')
+    assert_equal nil, status
   end
 
 end
